53108eb3e149ee2e48b6925d4523aafb27d45a1b
Mannyg448 <71471185+Mannyg448@users.noreply.github.com>
2022-04-03 23:06:31 -0500

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,54 @@
+<script setup>
+import {useRouter} from "vue-router";
+import useAuth from "../composable/useAuth";
+const {isAuthenticated, logout} = useAuth();
+
+const router = useRouter();
+
+const loggingOut = () => {
+    logout();
+    router.push("/");
+}
+
+</script>
+
+
+
+
+<template>
+   <div class="bg-green-800 text-yellow-300">
+        <div class="container mx-auto flex items-center italic justify-between ">
+            <h1 class="tracking-tighter text-3xl font-semibold">
+                Vue<span class="font-normal">RealAuth</span>
+                </h1>
+            <nav>
+                <ul class="flex space-x-4">
+                    <router-link to="/">
+                    <li class="py-8 px-4  hover:cursor-pointer hover:bg-yellow-300 hover:text-green-800">
+                        Home
+                        </li></router-link>
+                     <router-link to="/about">   
+                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-green-800">About</li>
+                    </router-link>
+                    <router-link v-if="!isAuthenticated" to="/login">
+                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-green-800">Login</li>
+                    </router-link>
+
+                    <div v-else class="flex">
+                     <router-link  to="/secret">
+                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-green-800">Secret</li>
+                    </router-link>
+
+
+                     <button  @click="loggingOut">
+                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-green-800">Logout</li>
+                    </button>
+                    </div>
+                    
+                </ul>
+
+            </nav>
+        </div>
+        </div>
+    
+</template>
---
+++ src/pages/login.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
+
+const schema = yup.object({
+      username: yup.string().required().email().label("Email"),
+      password: yup.string().required().min(8).label("Password"),
+    });
+
+useForm({
+      validationSchema: schema,
+    });
+
+    const { value: username, errorMessage: emailError } = useField("username");
+    const { value: password, errorMessage: passwordError } = useField("password");
+    
+
+import useAuth from "../composable/useAuth";
+import useError from "../composable/useError";
+
+
+const { isAuthenticated, login, signup, googleLogin } = useAuth();
+// const username = ref("");
+// const password = ref("");
+
+const router = useRouter();
+
+const logginIn = async () => {
+    await login(username.value, password.value);
+    goToHome();
+};
+
+const signingUp = async () => {
+    await signup(username.value, password.value);
+    goToHome();
+};
+
+const google = async () => {
+    await googleLogin();
+    goToHome();
+};
+const goToHome = () => {
+    if (isAuthenticated.value) {
+        router.push("/");
+    } else {
+        setError("Invalid username or password");
+        start();
+    }
+};
+
+const {error, setError } = useError();
+import { promiseTimeout, useTimeout } from "@vueuse/core";
+
+const { ready, start } = useTimeout(3000, { controls: true });
+
+</script>
+
+
+
+
+<template>
+<div class="flex justify-center flex-col space-y-12 items-center min-h-screen-nonav">
+<div class=" bg-gray-300 shadow-2xl justify-center overflow-hidden items-center rounded-lg flex">
+<img class="h-64" src="../assets/bglogin.png" alt="Hello BG">
+<form @submit.prevent="logginIn" class="flex flex-col space-y-4 p-4">
+    <input
+    name="username"
+     type="text" class="border-2 p-2 rounded-lg " placeholder="Email" v-model="username" />
+     <span class="text-red-500 text-center text-xs"> {{emailError}}</span>
+
+    <input
+    name="password"
+     type="password" class="border-2 p-2 rounded-lg" placeholder="Password" v-model="password" />
+     <span class="text-red-500 text-center text-xs"> {{passwordError}}</span>
+
+    <div class="flex space-x-2">
+    <button type="submit" @submit.prevent="logginIn" class="bg-pink-500 rounded-lg w-1/2 text-gray-800 py-2">Login</button>
+
+    <button  @click="signingUp" class="bg-green-800 rounded-lg w-1/2 text-yellow-400 py-2">Signup</button>
+    </div>
+
+<button 
+    @click="google"
+    class="bg-white flex justify-center py-2 rounded-lg hover:bg-green-400"> 
+    <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"></button>
+</form>
+</div>
+<div
+v-if="!ready && error"
+ class="bg-red-300  w-1/4 p-4 text-red-800 rounded-lg absolute bottom-2 right-2 text-center">
+    {{error}}
+</div>
+</div>
+</template>
